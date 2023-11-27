8c8db01dba271b2771fd0562b2485c9741f932db
kmg22 <ssukmg22@gmail.com>
2023-11-27 23:08:47 +0900

    meeting with github

--- hi.py
+++ hi.py
@@ -1,11 +1,13 @@
 print("hi")
 
+# <github organization의 초기 레포 연동>
 # git init
 # git remote add origin https://github.com/tutoringPractice/hi_github.git
 # git remote add .
 # git commit -m "hi"
 # git push origin master
 # --------------
+# <branch를 판다면?>
 # git checkout -b bye
 
 print("bye")
@@ -13,5 +15,6 @@ print("bye")
 # git add .
 # git commit -m "meeting with github"
 # git push origin bye
+# -> github으로 pr 날림. merge하면 origin에 반영
 # git checkout master : "print("hi")만 남겠지?
 # git pull origin master : print("bye")도 나타나겠지?
